ca884db2213dedf15be6f4c6745327d91faa860b
Justin C. Scott & Kyle Mellander <student@epicodus.com>
2015-09-01 15:00:13 -0700

    Add food price to database

--- lib/city.rb
+++ lib/city.rb
@@ -5,7 +5,7 @@ class City < ActiveRecord::Base
 
   def city_data
     @doc = Nokogiri::HTML(open("http://www.numbeo.com/hotel-prices/city_result.jsp?country=#{country_name.gsub(/ /, "+")}&city=#{city_name.gsub(/, /, "%2C+").gsub(/ /, "+")}&displayCurrency=USD"))
-    self.update({daily_average_cost: daily_average_cost_calc})
+    self.update({daily_average_cost: daily_average_cost_calc, food_cost: food_price_calc})
     # accomodation = Accomodation.find_by(city_id: city.id)
     # accomodation.update({one_star_cost: hotel_prices[0], two_star_cost: hotel_prices[1], three_star_cost: hotel_prices[2], four_star_cost: hotel_prices[3], five_star_cost: hotel_prices[4]})
   end
@@ -33,7 +33,7 @@ class City < ActiveRecord::Base
     i = 1
     @doc.css('.data_wide_table td').each do |price|
       if i < 8
-        food_price += price
+        food_price += price.text.to_f
       end
       i += 1
     end
--- spec/cost_spec.rb
+++ spec/cost_spec.rb
@@ -7,6 +7,11 @@ describe City do
     expect(portland.daily_average_cost).to eq(275.48)
   end
 
+  it 'will fetch the price of a hotel from numbeo' do
+    portland = City.create({city_name: "Portland, OR", country_name: "United States", daily_average_cost: 0.00})
+    portland.city_data
+    expect(portland.food_cost).to eq(62.88)
+  end
   # it 'will fetch the price of a hotel from numbeo' do
   #   test_array = ["Portland, OR", "United States"]
   #   expect(test_array.tourist_average_cost).to eq("275.48")
