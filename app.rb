5c83977c036d0c05a6d447f23f260a20e87187b4
Justin Scott <student@epicodus.com>
2015-09-02 10:09:41 -0700

    Return results given search parameters

--- app.rb
+++ app.rb
@@ -9,5 +9,9 @@ end
 
 post '/' do
   budget = params['budget'].to_f
-  erb(:index)
+  city_name = params['city_name']
+  vacation_length = params['vacation_length']
+  input = Input.new({budget: budget, city_name: city_name, vacation_length: vacation_length})
+  @cities = input.list_vacations
+  erb(:vacations)
 end
