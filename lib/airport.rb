0673d550165f11e12f397e7bdfb827d9daa87a62
ryan heupler <student@epicodus.com>
2015-09-02 11:18:50 -0700

    adds method for flight information

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,16 @@
 class Airport < ActiveRecord::Base
   belongs_to(:cities)
+  require "httparty"
+
+  # attr_accessor :cost
+  #
+  # def initialize(cost)
+  #   self.cost = cost
 
   def airport_data
-    @doc = Nokogiri::HTML(open("http://www.numbeo.com/hotel-prices/city_result.jsp?country=#{country_name.gsub(/ /, "+")}&city=#{city_name.gsub(/, /, "%2C+").gsub(/ /, "+")}&displayCurrency=USD"))
-    self.update({daily_average_cost: daily_average_cost_calc, food_cost: food_price_calc, one_star_cost: hotel_prices[0], two_star_cost: hotel_prices[1], three_star_cost: hotel_prices[2], four_star_cost: hotel_prices[3], five_star_cost: hotel_prices[4]})
+    answer = HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDqAnFxbvWwhyBZEGsZYN1lfGmIoOCE77k",
+    :body => { request: { passengers: { adultCount: 1}, slice: [{origin: "PDX", destination: "SEA", date: "2015-09-25"},{origin: "SEA", destination: "PDX", date: "2015-10-10"}]}}.to_json,
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+    answer
   end
 end
